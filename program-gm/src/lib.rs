f9aacbcd5772dcb2bc5d9e3d698dd24c5a0b5acc
jhondev <jma9974@gmail.com>
2022-05-11 13:51:36 -0500

    gm program

--- program-gm/src/lib.rs
+++ program-gm/src/lib.rs
@@ -1,8 +1,52 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+};
+
+// Program Id: ENsPqjh93Aer8vars1k9VUAUAFrpTGZea1zwaymqkDV
+
+// Define the type of state stored in accounts
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+pub struct GreetingAccount {
+    pub name: String,
+}
+
+// Declare and export the program's entrypoint
+entrypoint!(process_instruction);
+
+// Program entrypoint's implementation
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    input: &[u8],
+) -> ProgramResult {
+    msg!("GM program entrypoint");
+
+    // Iterating accounts is safer than indexing
+    let accounts_iter = &mut accounts.iter();
+
+    // Get the account to say GM to
+    let account = next_account_info(accounts_iter)?;
+
+    // The account must be owned by the program in order to modify its data
+    if account.owner != program_id {
+        msg!("Greeted account does not have the correct program id");
+        return Err(ProgramError::IncorrectProgramId);
     }
+
+    // Deserialize the input data, and store it in a GreetingAccount struct
+    let input_data = GreetingAccount::try_from_slice(&input).unwrap();
+
+    // Say GM in the Program output
+    msg!("GM {}", input_data.name);
+
+    // Serialize the name, and store it in the  passed in account
+    input_data.serialize(&mut &mut account.try_borrow_mut_data()?[..])?;
+
+    Ok(())
 }
